cdcfb2e1370e7411a3ad1b1cb1abb7b603ac907e
Cam-illa <81303399+Cam-illa@users.noreply.github.com>
2021-05-01 13:37:37 +0700

    Возврат в Меню

--- app/src/main/java/com/example/uncleanpower/SecondActivity.kt
+++ app/src/main/java/com/example/uncleanpower/SecondActivity.kt
@@ -2,10 +2,17 @@ package com.example.uncleanpower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+    }
+
+    fun toMenu (view: View) {
+        val menu = Intent(this, MainActivity::class.java)
+        startActivity(menu)
     }
 }
