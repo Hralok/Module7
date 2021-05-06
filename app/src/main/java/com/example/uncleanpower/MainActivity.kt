9d796d34addca6b41d162f30e367ef3596c40b13
Hralok <hralok@list.ru>
2021-05-06 14:09:34 +0300

    Косметические улучшения

--- app/src/main/java/com/example/uncleanpower/MainActivity.kt
+++ app/src/main/java/com/example/uncleanpower/MainActivity.kt
@@ -5,37 +5,58 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
-val CAMERA_RQ = 101
-val STORAGE_RQ = 102
+const val CAMERA_RQ = 101
+const val STORAGE_RQ = 102
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttonTap()
+
+
+
+
     }
 
-    private fun checkPerm (permission: String, requestCode: Int) {
-        when {ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED -> {
-            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+    private fun checkPerm (permission: String, requestCode: Int):Boolean {
+        /*
+        Проверяет наличие разрешения на использование камеры/галереи, в случае наличия разрешения возвращает значение true,
+        в противном случае запрашивает разрешение у пользователя после чего возвращает true или false в зависимости от того, было
+        ли получено разрешение
+        */
+        return if (ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED) {
+            true
         }
-        //shouldShowRequestPermissionRationale(permission)
-        else -> ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+        else
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+            ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED
+        }
     }
-        }
 
     private fun buttonTap() {
         gal_butt.setOnClickListener {
-            checkPerm(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_RQ)
+            if (checkPerm(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_RQ))
+            {
+                var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            }
         }
         cam_butt.setOnClickListener {
-            checkPerm(Manifest.permission.CAMERA, CAMERA_RQ)
+            if (checkPerm(Manifest.permission.CAMERA, CAMERA_RQ))
+            {
+
+
+
+            }
         }
     }
 
