35375f2a7ce5b0035af6c66d601bcd757c0993e9
Cam-illa <81303399+Cam-illa@users.noreply.github.com>
2021-05-07 20:36:39 +0700

    Попытка в камеру

---
+++ app/src/main/java/com/example/uncleanpower/CameraFragment.kt
@@ -0,0 +1,110 @@
+package com.example.uncleanpower
+
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
+import androidx.activity.result.contract.ActivityResultContracts.TakePicturePreview
+import androidx.activity.result.launch
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import kotlinx.android.synthetic.main.fragment_camera.*
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class CameraFragment : Fragment(R.layout.fragment_camera) {
+
+    private val cameraPermission = registerForActivityResult(RequestPermission()) { granted ->
+        when {
+            granted -> {
+                // user granted permission
+                cameraShot.launch()
+            }
+//            !shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+//                // user denied permission and set Don't ask again.
+////                showSettingsDialog()
+//            }
+//            else -> {
+////                showToast(R.string.denied_toast)
+//            }
+        }
+    }
+
+    private val cameraShot = registerForActivityResult(TakePicturePreview()) { bitmap ->
+        if (bitmap != null) {
+            imageContainer.setImageBitmap(bitmap)
+            setImageIsVisible(true)
+        }
+//        else {
+//            // something was wrong
+////            showToast(R.string.something_wrong)
+//        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        cameraButton.setOnClickListener {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                // we need to tell user why do we need permission
+//                showRationaleDialog()
+            } else {
+                cameraPermission.launch(Manifest.permission.CAMERA)
+            }
+        }
+
+        closeButton.setOnClickListener {
+            setImageIsVisible(false)
+        }
+
+        setFragmentResultListener(RATIONALE_KEY) { _, bundle ->
+            val isWantToAllowAfterRationale = bundle.getBoolean(RESULT_KEY)
+            if (isWantToAllowAfterRationale) {
+                cameraPermission.launch(Manifest.permission.CAMERA)
+            }
+        }
+        setFragmentResultListener(SETTINGS_KEY) { _, bundle ->
+            val isWantToOpenSettings = bundle.getBoolean(RESULT_KEY)
+            if (isWantToOpenSettings) {
+                openSettings()
+            }
+        }
+    }
+
+    private fun showToast(textId: Int) {
+        Toast.makeText(context, textId, Toast.LENGTH_SHORT).show()
+    }
+
+//    private fun showRationaleDialog() {
+//        RationaleFragment().show(parentFragmentManager, RationaleFragment.TAG)
+//    }
+//
+//    private fun showSettingsDialog() {
+//        DontAskAgainFragment().show(parentFragmentManager, DontAskAgainFragment.TAG)
+//    }
+
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                .setData(Uri.fromParts("package", requireContext().packageName, null))
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    private fun setImageIsVisible(isVisible: Boolean) {
+        cameraButton.isVisible = !isVisible
+        closeButton.isVisible = isVisible
+        imageContainer.isVisible = isVisible
+    }
+
+    companion object {
+        const val RATIONALE_KEY = "rationale_tag"
+        const val SETTINGS_KEY = "settings_tag"
+        const val RESULT_KEY = "camera_result_key"
+    }
+}
--- app/src/main/java/com/example/uncleanpower/MainActivity.kt
+++ app/src/main/java/com/example/uncleanpower/MainActivity.kt
@@ -8,64 +8,101 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val CAMERA_RQ = 101
-const val STORAGE_RQ = 102
+//const val CAMERA_RQ = 101
+//const val STORAGE_RQ = 102
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        buttonTap()
 
-
-
-
-    }
-
-    private fun checkPerm (permission: String, requestCode: Int):Boolean {
-        /*
-        Проверяет наличие разрешения на использование камеры/галереи, в случае наличия разрешения возвращает значение true,
-        в противном случае запрашивает разрешение у пользователя после чего возвращает true или false в зависимости от того, было
-        ли получено разрешение
-        */
-        return if (ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED) {
-            true
-        }
-        else
-        {
-            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
-            ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED
-        }
-    }
-
-    private fun buttonTap() {
-        gal_butt.setOnClickListener {
-            if (checkPerm(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_RQ))
-            {
-                var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-            }
-        }
         cam_butt.setOnClickListener {
-            if (checkPerm(Manifest.permission.CAMERA, CAMERA_RQ))
-            {
-
-
-
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                // объясняем пользователю, почему нам необходимо данное разрешение
+            } else {
+                permission.launch(Manifest.permission.CAMERA)
             }
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    val permission = registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+        when {
+            granted -> {
+                camera.launch(imageUri)// доступ к камере разрешен, открываем камеру
+            }
+            !shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                // доступ к камере запрещен, пользователь поставил галочку Don't ask again.
+            }
+            else -> {
+                // доступ к камере запрещен
+            }
+        }
     }
 
-    fun secondScr (view: View) {
-        val secondS = Intent(this, SecondActivity::class.java)
-        startActivity(secondS)
+    val camera = registerForActivityResult(ActivityResultContracts.TakePicture()) { uri ->
+        // используем bitmap
     }
+
+
+
+
+
+
+
+
+
+
+
+
+//    private fun checkPerm (permission: String, requestCode: Int):Boolean {
+//        /*
+//        Проверяет наличие разрешения на использование камеры/галереи, в случае наличия разрешения возвращает значение true,
+//        в противном случае запрашивает разрешение у пользователя после чего возвращает true или false в зависимости от того, было
+//        ли получено разрешение
+//        */
+//        return if (ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED) {
+//            true
+//        }
+//        else
+//        {
+//            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+//            ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED
+//        }
+//    }
+//
+//    private fun buttonTap() {
+//        gal_butt.setOnClickListener {
+//            if (checkPerm(Manifest.permission.READ_EXTERNAL_STORAGE, STORAGE_RQ))
+//            {
+//                var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//
+//            }
+//        }
+//        cam_butt.setOnClickListener {
+//            if (checkPerm(Manifest.permission.CAMERA, CAMERA_RQ))
+//            {
+//
+//
+//
+//            }
+//        }
+//    }
+//
+//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//    }
+
+
+
+
+//    fun cameraScr (view: View) {
+//        val secondS = Intent(this, CameraActivity::class.java)
+//        startActivity(secondS)
+//    }
 }
