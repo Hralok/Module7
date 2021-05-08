1a09fed3875cb726444fa3dd9661c9a289e6ddab
Cam-illa <81303399+Cam-illa@users.noreply.github.com>
2021-05-08 12:46:56 +0700

    Цветовая коррекция 2.0

--- app/src/main/java/com/example/uncleanpower/ColorCorrection.kt
+++ app/src/main/java/com/example/uncleanpower/ColorCorrection.kt
@@ -7,49 +7,55 @@ import kotlin.math.*
 
 class ColorCorrection() {
 
-    fun correction(bitmap: Bitmap) {
-        val hei = bitmap.height
-        val wi = bitmap.width
+    fun corr(originalBitmap: Bitmap?): Bitmap? {
+        var bitmap = originalBitmap?.copy( Bitmap.Config.ARGB_8888 , true)
+        bitmap?.apply {
+            val hei = bitmap.height
+            val wi = bitmap.width
 
-        val redColors = arrayOf(255, 0)
-        val greenColors = arrayOf(255, 0)
-        val blueColors = arrayOf(255, 0)
+            val redColors = arrayOf(255, 0)
+            val greenColors = arrayOf(255, 0)
+            val blueColors = arrayOf(255, 0)
 
-        for (i in 0 until wi) {
-            for (j in 0 until hei) {
+            for (i in 0 until wi) {
+                for (j in 0 until hei) {
 
-                val color = bitmap.getPixel(i, j)
-                val r = Color.red(color)
-                val g = Color.green(color)
-                val b = Color.blue(color)
+                    val color = bitmap.getPixel(i, j)
+                    val r = Color.red(color)
+                    val g = Color.green(color)
+                    val b = Color.blue(color)
 
-                redColors[0] = min(redColors[0], r)
-                redColors[1] = max(redColors[1], r)
+                    redColors[0] = min(redColors[0], r)
+                    redColors[1] = max(redColors[1], r)
 
-                greenColors[0] = min(greenColors[0], g)
-                greenColors[1] = max(greenColors[1], g)
+                    greenColors[0] = min(greenColors[0], g)
+                    greenColors[1] = max(greenColors[1], g)
 
-                blueColors[0] = min(blueColors[0], b)
-                blueColors[1] = max(blueColors[1], b)
+                    blueColors[0] = min(blueColors[0], b)
+                    blueColors[1] = max(blueColors[1], b)
+                }
             }
+
+            for (i in 0 until wi) {
+                for (j in 0 until hei) {
+
+                    val color = bitmap.getPixel(i, j)
+                    val r = Color.red(color)
+                    val g = Color.green(color)
+                    val b = Color.blue(color)
+                    val a = Color.alpha(color)
+
+                    val nr = (r - redColors[0]) * (255 - 0) / (redColors[1] - redColors[0])
+                    val ng = (g - greenColors[0]) * (255 - 0) / (greenColors[1] - greenColors[0])
+                    val nb = (b - blueColors[0]) * (255 - 0) / (blueColors[1] - blueColors[0])
+
+                    bitmap.setPixel(i, j, Color.argb(a, nr, ng, nb))
+                }
+            }
+
         }
 
-        for (i in 0 until wi) {
-            for (j in 0 until hei) {
-
-                val color = bitmap.getPixel(i, j)
-                val r = Color.red(color)
-                val g = Color.green(color)
-                val b = Color.blue(color)
-                val a = Color.alpha(color)
-
-                val nr = (r - redColors[0]) * (255 - 0) / (redColors[1] - redColors[0])
-                val ng = (g - greenColors[0]) * (255 - 0) / (greenColors[1] - greenColors[0])
-                val nb = (b - blueColors[0]) * (255 - 0) / (blueColors[1] - blueColors[0])
-
-                bitmap.setPixel(i, j, Color.argb(a, nr, ng, nb))
-            }
-        }
+        return bitmap
 
     }
 }
